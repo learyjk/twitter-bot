e36f9964e211852ce9ed77d3169c266ead53cf65
keeganleary <leary.keegan@gmail.com>
2020-11-27 20:50:28 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from config import TWITTER_USER, TWITTER_PASS
+from time import sleep
+
+chrome_driver_path = "/Users/keeganleary/Development/chromedriver"
+PROMISED_DOWN = 150
+PROMISED_UP = 10
+
+class InternetSpeedTwitterBot:
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(driver_path)
+        self.up = 0
+        self.down = 0
+
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+        sleep(3)
+        go_button = self.driver.find_element_by_css_selector(".start-button a")
+        go_button.click()
+
+        sleep(60)
+        self.down = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        self.up = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+
+
+    def tweet_at_isp(self):
+        self.driver.get("https://twitter.com/login")
+        sleep(2)
+        user = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
+        password = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
+
+        user.send_keys(TWITTER_USER)
+        password.send_keys(TWITTER_PASS)
+
+        sleep(2)
+        password.send_keys(Keys.ENTER)
+
+        sleep(5)
+        tweet_compose = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        sleep(3)
+
+        tweet_button = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]/div/span/span')
+        tweet_button.click()
+
+        sleep(2)
+        self.driver.quit()
+
+
+bot = InternetSpeedTwitterBot(chrome_driver_path)
+bot.get_internet_speed()
+bot.tweet_at_isp()
